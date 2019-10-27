5605d94e03b535eee82ab6e1ebe9f1dc661cbfef
Tona Diaz <tdiaz@healthwise.org>
2019-10-26 20:13:43 -0600

    Added deployments for frontend and backend Kubernetes.

--- build_infrastructure.ps1
+++ build_infrastructure.ps1
@@ -4,3 +4,17 @@ $containerRegistry = 'MovieRatingAppContainerRegistry'
 az group create --name $resourceGroup --location westus
 az acr create --resource-group $resourceGroup --name $containerRegistry --admin-enabled --sku Basic 
 az configure --defaults acr=$containerRegistry
+az aks create --resource-group $resourceGroup --name KubeCluster --node-count 8 --node-vm-size Standard_B2s --enable-addons monitoring --generate-ssh-keys
+
+# Install Kubernetes CLI (kubectl)
+az aks install-cli
+
+# Create local configuration file to talk to the AKS Cluster
+az aks get-credentials --resource-group $resourceGroup --name KubeCluster
+
+# Kubernetes Dashboard & permissions
+az aks browse --resource-group $resourceGroup --name KubeCluster
+kubectl create clusterrolebinding kubernetes-dashboard --clusterrole=cluster-admin --serviceaccount=kube-system:kubernetes-dashboard
+
+# Attach the ACR Instance to the Cluster
+az aks update -n KubeCluster -g $resourceGroup --attach-acr movieratingappcontainerregistry
--- frontend.Dockerfile
+++ frontend.Dockerfile
@@ -7,6 +7,6 @@ RUN npm run build
 
 FROM nginx as production-stage
 RUN mkdir /app
-ENV backendUrl=http://movierating-backend/
+ENV backendUrl=http://movierating-backend:8080
 COPY --from=build-stage /app/target/dist /app
 COPY frontend/nginx.conf /etc/nginx/nginx.conf
